5506d64ee05e03ed7a5099f0055d0cf037a6d505
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-10-23 16:56:00 +0530

    Adding styled navbar for BadBank

--- nav.js
+++ nav.js
@@ -2,19 +2,39 @@ function Nav() {
   return (
     <ul className="nav">
       <li className="nav-item">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
+        <a className="nav-link" href="#">
+          BadBank
+        </a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/about/">
-          About
-        </Link>
+        <a className="nav-link" href="#/CreateAccount">
+          Create Account
+        </a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/products">
-          Products
-        </Link>
+        <a className="nav-link" href="#/Login">
+          Login
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#/Deposit">
+          Deposit
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#/Withdraw">
+          Withdraw
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#/Balance">
+          Balance
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#/AllData">
+          AllData
+        </a>
       </li>
     </ul>
   );
