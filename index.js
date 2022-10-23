c133144a0ac88e0a621f848ddcd7b5cdfd1031a7
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-10-23 16:27:54 +0530

    Adding Bootstrap style NavBar

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const UserContext = React.createContext(null);
-function Spa() {
-  const Route = ReactRouterDOM.Route;
-  const Link = ReactRouterDOM.Link;
-  const HashRouter = ReactRouterDOM.HashRouter;
+const Route = ReactRouterDOM.Route;
+const Link = ReactRouterDOM.Link;
+const HashRouter = ReactRouterDOM.HashRouter;
 
+function Spa() {
   return (
     <HashRouter>
       <div>
         <h1>Routing - Hello World!</h1>
-        <Link to="/">Home</Link> --
-        <Link to="/about">About</Link> --
-        <Link to="/products">Products</Link>
+        <Nav></Nav>
         <hr />
         <UserContext.Provider value={{ users: ["John"] }}>
           <Route path="/" exact component={Home} />
