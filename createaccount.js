7dab9c995fb46e488f5ad657639aad0e8a929583
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-10-28 14:29:08 +0530

    Adding login link

--- createaccount.js
+++ createaccount.js
@@ -32,63 +32,72 @@ function CreateAccount() {
   }
 
   return (
-    <Card
-      bgcolor={show ? "secondary" : "success"}
-      header="Create Account"
-      status={status}
-      body={
-        show ? (
-          <>
-            Name
-            <br />
-            <input
-              type="input"
-              id="name"
-              className="form-control"
-              placeholder="Enter name.."
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-            ></input>
-            <br />
-            Email address
-            <br />
-            <input
-              type="input"
-              id="email"
-              className="form-control"
-              placeholder="Enter email.."
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            ></input>
-            <br />
-            Password
-            <br />
-            <input
-              type="input"
-              id="password"
-              className="form-control"
-              placeholder="Enter password.."
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            ></input>
-            <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleCreate}
-            >
-              Create Account
-            </button>
-          </>
-        ) : (
-          <>
-            <h5>Success! You have created an account</h5>
-            <button type="submit" className="btn btn-light" onClick={clearForm}>
-              Add another account
-            </button>
-          </>
-        )
-      }
-    ></Card>
+    <>
+      <Card
+        bgcolor={show ? "secondary" : "success"}
+        header="Create Account"
+        status={status}
+        body={
+          show ? (
+            <>
+              Name
+              <br />
+              <input
+                type="input"
+                id="name"
+                className="form-control"
+                placeholder="Enter name.."
+                value={name}
+                onChange={(e) => setName(e.currentTarget.value)}
+              ></input>
+              <br />
+              Email address
+              <br />
+              <input
+                type="input"
+                id="email"
+                className="form-control"
+                placeholder="Enter email.."
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              ></input>
+              <br />
+              Password
+              <br />
+              <input
+                type="input"
+                id="password"
+                className="form-control"
+                placeholder="Enter password.."
+                value={password}
+                onChange={(e) => setPassword(e.currentTarget.value)}
+              ></input>
+              <br />
+              <button
+                type="submit"
+                className="btn btn-light"
+                onClick={handleCreate}
+              >
+                Create Account
+              </button>
+            </>
+          ) : (
+            <>
+              <h5>Success! You have created an account</h5>
+              <button
+                type="submit"
+                className="btn btn-light"
+                onClick={clearForm}
+              >
+                Add another account
+              </button>
+            </>
+          )
+        }
+      ></Card>
+      <h6>
+        Already have an account? <a href="#Login">Log in</a>
+      </h6>
+    </>
   );
 }
