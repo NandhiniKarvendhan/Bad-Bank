a01f85d00714dbed9b37bb04d6bff49943d4a51d
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-10-28 14:35:28 +0530

    Adding login form

--- login.js
+++ login.js
@@ -1,3 +1,37 @@
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <Card
+        bgcolor="primary"
+        header="Login to your account"
+        body={
+          <>
+            Email
+            <br />
+            <input
+              type="input"
+              id="email"
+              className="form-control"
+              placeholder="Enter email.."
+            ></input>
+            <br />
+            Password <br />
+            <input
+              type="input"
+              id="password"
+              className="form-control"
+              placeholder="Enter password.."
+            ></input>
+            <br />
+            <button type="submit" className="btn btn-outline-light">
+              Login
+            </button>
+          </>
+        }
+      ></Card>
+      <h6>
+        Don't have an account?<a href="#CreateAccount">Sign up</a>{" "}
+      </h6>
+    </>
+  );
 }
