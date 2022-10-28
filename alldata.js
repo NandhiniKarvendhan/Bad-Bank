2b6e09fc6a6161e32b5ec362a123ed5f5f5e6fc0
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-10-28 14:30:48 +0530

    updating all data component

--- alldata.js
+++ alldata.js
@@ -2,7 +2,8 @@ function AllData() {
   const ctx = React.useContext(UserContext);
   return (
     <>
-      <h1>AllData</h1>;{JSON.stringify(ctx)};
+      <h1>AllData</h1>
+      {JSON.stringify(ctx)};
     </>
   );
 }
