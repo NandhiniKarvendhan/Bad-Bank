49c8c54087e9ecbbe09631d1b9de072930254423
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-01 15:42:30 +0530

    Updating login form

--- login.js
+++ login.js
@@ -1,37 +1,10 @@
 function Login() {
   return (
-    <>
-      <Card
-        bgcolor="primary"
-        header="Login to your account"
-        body={
-          <>
-            Email
-            <br />
-            <input
-              type="input"
-              id="email"
-              className="form-control"
-              placeholder="Enter email.."
-            ></input>
-            <br />
-            Password <br />
-            <input
-              type="input"
-              id="password"
-              className="form-control"
-              placeholder="Enter password.."
-            ></input>
-            <br />
-            <button type="submit" className="btn btn-outline-light">
-              Login
-            </button>
-          </>
-        }
-      ></Card>
+    <div className="form-container">
+      <BankForm bgcolor="secondary" header="Login"></BankForm>
       <h6>
-        Don't have an account?<a href="#CreateAccount">Sign up</a>{" "}
+        Don't have an account?<a href="#CreateAccount">Sign up</a>
       </h6>
-    </>
+    </div>
   );
 }
