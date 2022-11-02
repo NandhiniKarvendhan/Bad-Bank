3d775814786bcdd24eb5ed23c84881b79d7381be
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-02 11:47:14 +0530

    Updating home component

--- home.js
+++ home.js
@@ -1,11 +1,17 @@
 function Home() {
   return (
-    <Card
-      txtcolor="black"
-      header="BadBank Landing Module"
-      title="Welcome to the bank"
-      text="You can move around using the navigation bar."
-      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
-    />
+    // <Card
+    //   txtcolor="black"
+    //   header="BadBank Landing Module"
+    //   title="Welcome to the bank"
+    //   text="You can move around using the navigation bar."
+    //   body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
+    // />
+
+    <>
+      <h1>Welcome to the bank</h1>
+      <p>You can move around using the navigation bar</p>
+      <img src="bank.png" className="img-fluid" />
+    </>
   );
 }
