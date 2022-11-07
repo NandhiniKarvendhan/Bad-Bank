e7bdc99823332996bc2c5554852e3fb7a51ffcf4
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-07 22:19:08 +0530

    Adding withdraw functionality

--- withdraw.js
+++ withdraw.js
@@ -1,3 +1,29 @@
 function Withdraw() {
-  return <h1>Withdraw</h1>;
+  return (
+    <div className="card-container">
+      <Card
+        bgcolor="dark"
+        txtcolor="white"
+        header="Withdraw"
+        title="Balance is zero"
+        body={
+          <>
+            <br />
+            Withdraw
+            <br />
+            <input
+              type="number"
+              id="amount"
+              className="form-control"
+              placeholder="Enter amount.."
+            ></input>
+            <br />
+            <button type="submit" className="btn btn-outline-light">
+              Withdraw
+            </button>
+          </>
+        }
+      ></Card>
+    </div>
+  );
 }
