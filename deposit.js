787c372049b851899adcedc06b00768f291467f1
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-11-14 15:22:45 +0530

    Updating Deposit component

--- deposit.js
+++ deposit.js
@@ -1,13 +1,14 @@
 function Deposit() {
-  const [amount, setAmount] = React.useState(0);
+  const ctx = React.useContext(UserContext);
+  let amt = JSON.stringify(ctx.users[0]["balance"]);
+  const [disabled, setDisabled] = React.useState(true);
   const [deposit, setDeposit] = React.useState("");
-  const [total, setTotal] = React.useState(0);
+  const [total, setTotal] = React.useState(amt);
   const [show, setShow] = React.useState(true);
   const handleChange = (event) => {
     setDeposit(event.target.value);
     event.preventDefault();
-
-    // isNaN() method for number returns false, for negative numbers is used
+    setDisabled(false);
     if (/^\d+$/.test(event.target.value) == true) {
       setDeposit(event.target.value);
       event.preventDefault();
@@ -18,59 +19,53 @@ function Deposit() {
   };
 
   const handleSubmit = (event) => {
-    setTotal(total + amount + Number(deposit));
+    setTotal(Number(total) + Number(deposit));
 
-    setAmount(total);
     event.preventDefault();
     setShow(false);
+    ctx.users.push({ balance: Number(deposit) + Number(total) });
   };
 
   function clearForm() {
-    setAmount(0);
     setDeposit("");
     setShow(true);
+    setDisabled(true);
   }
   return (
     <div className="card-container">
       <Card
-        bgcolor="secondary"
+        bgcolor={show ? "secondary" : "success"}
         txtcolor="white"
         header="Deposit"
         body={
           show ? (
             <>
-              <h2>Balance is {total}</h2>
-              <br />
-              Deposit
-              <br />
-              <input
-                type="input"
-                id="amount"
-                className="form-control"
+              <h2>Balance is ${total}</h2>
+              <Form
+                label1="Deposit"
+                type1="input"
+                id1="amount"
                 placeholder="Enter amount.."
-                value={deposit}
-                onChange={handleChange}
-              ></input>
-              <br />
-              <button
-                type="submit"
-                className="btn btn-outline-light"
+                value1={deposit}
+                onChange1={handleChange}
+                type2="hidden"
+                type3="hidden"
+                button="Deposit"
                 onClick={handleSubmit}
-              >
-                Deposit
-              </button>{" "}
+                disabled={disabled}
+              ></Form>
             </>
           ) : (
             <>
-              <h5>Success! You have deposited {deposit}</h5>
+              <h6>Success! You have deposited ${deposit}</h6>
               <h2>Balance is {total}</h2>
-              <button
-                type="submit"
-                className="btn btn-light"
+              <Form
+                type1="hidden"
+                type2="hidden"
+                type3="hidden"
                 onClick={clearForm}
-              >
-                Deposit Again
-              </button>
+                button="Deposit Again"
+              ></Form>
             </>
           )
         }
