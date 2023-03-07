142c8f508ea7c516fae60f0cbbc99c688971aac8
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-03-07 20:47:30 +0530

    updating index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require("express");
 var app = express();
 var cors = require("cors");
-
+var dal = require("./dal.js");
 // used to serve static files from public directory
 app.use(express.static("public"));
 app.use(cors());
 
 app.get("/account/create/:name/:email/:password", (req, res) => {
-  console.log(req.params);
-  const name = req.params.name;
-  const email = req.params.email;
-  const password = req.params.password;
-  res.send({ name, email, password, balance: 0 });
+  dal
+    .create(req.params.name, req.params.email, req.params.password)
+    .then((user) => {
+      console.log(user);
+      res.send(user);
+    });
 });
+
+app.get("/account/all", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Running on port 3000!");
