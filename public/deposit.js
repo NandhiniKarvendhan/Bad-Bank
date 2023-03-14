ecc19b5bf3f572bb3eb9422c812c98e35520885c
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-03-14 15:14:48 +0530

    updating deposit component

--- public/deposit.js
+++ public/deposit.js
@@ -1,33 +1,46 @@
 function Deposit() {
-  const ctx = React.useContext(UserContext);
-  let amt = JSON.stringify(ctx.users[0]["balance"]);
+  // let amt = JSON.stringify(ctx.users[0]["balance"]);
+  const [email, setEmail] = React.useState("");
   const [disabled, setDisabled] = React.useState(true);
-  const [deposit, setDeposit] = React.useState("");
-  const [total, setTotal] = React.useState(amt);
+  // const [deposit, setDeposit] = React.useState("");
+  const [amount, setAmount] = React.useState();
   const [show, setShow] = React.useState(true);
-  const handleChange = (event) => {
-    setDeposit(event.target.value);
-    event.preventDefault();
-    setDisabled(false);
-    if (/^\d+$/.test(event.target.value) == true) {
-      setDeposit(event.target.value);
-      event.preventDefault();
-    } else {
-      setDeposit("");
-      return alert("Enter positive Number");
-    }
-  };
+  // const handleChange = (event) => {
+  //   setDeposit(event.target.value);
+  //   event.preventDefault();
+  //   setDisabled(false);
+  //   if (/^\d+$/.test(event.target.value) == true) {
+  //     setDeposit(event.target.value);
+  //     event.preventDefault();
+  //   } else {
+  //     setDeposit("");
+  //     return alert("Enter positive Number");
+  //   }
+  // };
 
   const handleSubmit = (event) => {
-    setTotal(Number(total) + Number(deposit));
+    // setTotal(Number(total) + Number(deposit));
+    fetch(`/account/update/${email}/${amount}`)
+      .then((response) => response.text())
+      .then((text) => {
+        try {
+          const data = JSON.parse(text);
 
+          console.log("JSON:", data);
+          // setStatus(" ");
+        } catch (err) {
+          console.log("err:", text);
+          // setStatus(text);
+        }
+      });
     event.preventDefault();
     setShow(false);
-    ctx.users.push({ balance: Number(deposit) + Number(total) });
+    // ctx.users.push({ balance: Number(deposit) + Number(total) });
   };
 
   function clearForm() {
-    setDeposit("");
+    setEmail("");
+
     setShow(true);
     setDisabled(true);
   }
@@ -40,15 +53,25 @@ function Deposit() {
         body={
           show ? (
             <>
-              <h2>Balance is ${total}</h2>
+              {/* <h2>Balance is ${total}</h2> */}
               <Form
-                label1="Deposit"
+                label1="Email"
                 type1="input"
-                id1="amount"
-                placeholder="Enter amount.."
-                value1={deposit}
-                onChange1={handleChange}
-                type2="hidden"
+                id1="email"
+                value1={email}
+                placeholder1="Enter email.."
+                onChange1={(e) => {
+                  setEmail(e.currentTarget.value);
+                  setDisabled(false);
+                }}
+                label2="Deposit"
+                type2="input"
+                id2="amount"
+                placeholder2="Enter amount.."
+                value2={amount}
+                onChange2={(e) => {
+                  setAmount(e.currentTarget.value);
+                }}
                 type3="hidden"
                 button="Deposit"
                 onClick={handleSubmit}
@@ -57,8 +80,8 @@ function Deposit() {
             </>
           ) : (
             <>
-              <h6>Success! You have deposited ${deposit}.</h6>
-              <h2>Balance is {total}</h2>
+              <h6>Success! You have deposited ${amount}.</h6>
+              {/* <h2>Balance is {total}</h2> */}
               <Form
                 type1="hidden"
                 type2="hidden"
