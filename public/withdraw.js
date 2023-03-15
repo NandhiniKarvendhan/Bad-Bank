0f6c7e28a774bef5357acfdf7bcb1dba65fd9c6b
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-03-15 15:56:43 +0530

    Updating withdraw component

--- public/withdraw.js
+++ public/withdraw.js
@@ -1,40 +1,38 @@
 function Withdraw() {
-  const ctx = React.useContext(UserContext);
-  let amt = JSON.stringify(ctx.users[0]["balance"]);
   const [disabled, setDisabled] = React.useState(true);
   const [withdraw, setWithdraw] = React.useState("");
-  const [total, setTotal] = React.useState(amt);
+  const [email, setEmail] = React.useState("");
   const [show, setShow] = React.useState(true);
 
-  const handleChange = (event) => {
-    setWithdraw(event.target.value);
-    event.preventDefault();
-    if (Number(event.target.value) > Number(total)) {
-      setWithdraw("");
-      setDisabled(true);
-      return alert("Balance is low");
-    } else {
-      setDisabled(false);
-    }
-    if (/^\d+$/.test(event.target.value) == true) {
-      setWithdraw(event.target.value);
-      event.preventDefault();
-    } else {
-      setWithdraw("");
-      return alert("Enter positive Number");
-    }
-  };
+  // const handleChange = (event) => {
+  //   setWithdraw(event.target.value);
+  //   event.preventDefault();
+  //   if (Number(event.target.value) > Number(total)) {
+  //     setWithdraw("");
+  //     setDisabled(true);
+  //     return alert("Balance is low");
+  //   } else {
+  //     setDisabled(false);
+  //   }
+  //   if (/^\d+$/.test(event.target.value) == true) {
+  //     setWithdraw(event.target.value);
+  //     event.preventDefault();
+  //   } else {
+  //     setWithdraw("");
+  //     return alert("Enter positive Number");
+  //   }
+  // };
 
   const handleSubmit = (event) => {
     {
-      setTotal(Number(total) - Number(withdraw));
+      // setTotal(Number(total) - Number(withdraw));
       console.log(Number(withdraw));
-      console.log(Number(total));
+      // console.log(Number(total));
 
       event.preventDefault();
       setShow(false);
     }
-    ctx.users.push({ balance: Number(total) - Number(withdraw) });
+    // ctx.users.push({ balance: Number(total) - Number(withdraw) });
   };
 
   function clearForm() {
@@ -51,16 +49,23 @@ function Withdraw() {
         body={
           show ? (
             <>
-              <h3>Balance is ${total}</h3>
               <Form
-                label1="Withdraw"
+                label1="Email"
                 type1="input"
-                id1="amount"
-                type2="hidden"
+                id1="email"
+                value1={email}
+                placeholder1="Enter email.."
+                onChange1={(e) => {
+                  setEmail(e.currentTarget.value);
+                  setDisabled(false);
+                }}
+                label2="Withdraw"
+                type2="input"
+                id2="amount"
+                placeholder2="Enter amount.."
+                value2={withdraw}
+                // onChange2={handleChange}
                 type3="hidden"
-                placeholder1="Enter amount.."
-                value1={withdraw}
-                onChange1={handleChange}
                 onClick={handleSubmit}
                 disabled={disabled}
                 button="Withdraw"
@@ -72,7 +77,7 @@ function Withdraw() {
                 Success! Withdraw was processed. <br /> You have withdrawn $
                 {withdraw}.
               </h6>
-              <h2>Balance is ${total}.</h2>
+              {/* <h2>Balance is ${total}.</h2> */}
               <Form
                 type1="hidden"
                 type2="hidden"
