197ea28354d51ac67d3a8e5ba5f95a400820ed7e
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-04-11 18:04:16 +0530

    Adding backend files

---
+++ server/dal.js
@@ -0,0 +1,59 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
+
+// connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+  console.log("Connected successfully to db server");
+
+  // connect to bad-bank database
+  db = client.db("bad-bank");
+});
+
+function create(name, email, password) {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection("accountHolders");
+    const doc = { name, email, password, balance: 0 };
+    collection.insertOne(doc, { w: 1 }, function (err, result) {
+      err ? reject(err) : resolve(doc);
+    });
+  });
+}
+
+function find(email) {
+  return new Promise((resolve, reject) => {
+    const customer = db
+      .collection("accountHolders")
+      .find({ email: email })
+      .toArray(function (err, docs) {
+        err ? reject(err) : resolve(docs);
+      });
+  });
+}
+
+function update(email, amount) {
+  return new Promise((resolve, reject) => {
+    const customer = db
+      .collection("accountHolders")
+      .findOneAndUpdate(
+        { email: email },
+        { $inc: { balance: amount } },
+        { returnNewDocument: false },
+        function (err, docs) {
+          err ? reject(err) : resolve(docs);
+        }
+      );
+  });
+}
+
+function all() {
+  return new Promise((resolve, reject) => {
+    const holders = db.collection("accountHolders").find({});
+    console.log(
+      holders.toArray(function (err, result) {
+        err ? reject(err) : resolve(result);
+      })
+    );
+  });
+}
+
+module.exports = { create, find, update, all };
---
+++ server/index.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var dal = require("./dal.js");
+
+// used to serve static files from build directory
+app.use(express.static("build"));
+app.use(cors());
+
+app.get("/account/create/:name/:email/:password", (req, res) => {
+  dal.find(req.params.email).then((user) => {
+    if (user.length > 0) {
+      res.send("The user already exists, Email is already taken.");
+    } else {
+      dal
+        .create(req.params.name, req.params.email, req.params.password)
+        .then((user) => {
+          console.log(user);
+          res.send(user);
+        });
+    }
+  });
+});
+app.get("/account/find/:email", (req, res) => {
+  dal.find(req.params.email).then((user) => {
+    res.send(user);
+  });
+});
+app.get("/account/login/:email/:password", (req, res) => {
+  dal.find(req.params.email).then((user) => {
+    if (user.length > 0) {
+      if (user[0].password === req.params.password) {
+        console.log(user);
+        res.send("Login successful! Welcome back " + user[0].name + ".");
+      } else {
+        res.send("Login failed: wrong password");
+      }
+    } else {
+      res.send("Login failed: Email not found.");
+    }
+  });
+});
+app.get("/account/update/:email/:amount", (req, res) => {
+  var amount = Number(req.params.amount);
+  dal.update(req.params.email, amount).then((response) => {
+    res.send(response);
+  });
+});
+
+app.get("/account/balance/:email", (req, res) => {
+  dal.find(req.params.email).then((user) => {
+    console.log(user);
+    if (user.length > 0) {
+      console.log(user.length);
+      res.send(
+        "Hi " + user[0].name + ". " + "Your balance is " + user[0].balance
+      );
+    } else {
+      res.send("Check your email address.");
+    }
+  });
+});
+app.get("/account/all", (req, res) => {
+  dal.all().then((user) => {
+    console.log(user);
+    res.send(user);
+  });
+});
+app.listen(3000, () => {
+  console.log("Running on port " + 3000 + "!");
+});
